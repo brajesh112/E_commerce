59a22161c610f219646fa025558b4b3cad17d9de
Brajesh Kumar <87276638+brajesh112@users.noreply.github.com>
2023-09-28 21:52:36 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,12 +75,5 @@ Rails.application.configure do
    config.action_mailer.perform_deliveries = true
    config.action_mailer.default :charset => "utf-8"
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-   address: 'smtp.gmail.com',
-   port: '587',
-   user_name: 'sahubrajesh112@gmail.com',
-   password: 'yzgsjdxhxvtyqoxx',
-   authentication: :plain,
-   enable_starttls_auto: true
-   }
+
 end
